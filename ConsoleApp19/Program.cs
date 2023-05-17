2c0dc6c815fb7dbeb6bef21ce86e48e9120f584a
RoMeRO93 <122970841+RoMeRO93@users.noreply.github.com>
2023-05-17 10:52:26 +0300

    Add project files.

---
+++ ConsoleApp19/Program.cs
@@ -0,0 +1,23 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        int numar1;
+        double numar2;
+        int numar3;
+        numar1 = 10;
+        numar2 = 5;
+        numar3 = 20;
+
+        if (numar1 == n10  && numar2 == 5  || numar3 == 20 )
+        {
+            // acest code
+            Console.WriteLine("ADEVARAT");
+        }
+        else
+        {
+            Console.WriteLine("FALS");
+        }
+      
+    }
+}
